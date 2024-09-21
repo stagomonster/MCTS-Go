b386c4180355a01bb2a78ab6cd7642ff7bceda9b
Staas Lin <stagomon@gmail.com>
2024-09-21 00:10:21 -0400

    added new mcts py file with basic structure for mcts implementation

---
+++ mcts_implementation.py
@@ -0,0 +1,20 @@
+import go_project
+import numpy as np
+
+class MCTS:
+    def __init__(state, parent=None): 
+        #TODO complete implementation of init mcts
+        self.board_state = state
+        self.parent = parent
+        self.children = []
+        pass
+    def expand(self):
+        pass
+    def backpropogate(self, result):
+        pass
+
+def main():
+    #TODO main function
+    pass
+    
+    
